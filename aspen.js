9db187af06f3f2550b57bd783c01a9ca64194b00
btmills <mills.brandont@gmail.com>
2013-01-27 06:15:57 -0500

    Rename to Aspen

--- aspen.js
+++ aspen.js
@@ -1,12 +1,12 @@
 define(['property', 'model', 'collection', 'databind'], function (Property, Model, Collection, Binding) {
 
-	var JPF = {
+	var Aspen = {
 		Binding: Binding,
 		Collection: Collection,
 		Model: Model,
 		Property: Property
 	};
 
-	return JPF;
+	return Aspen;
 
 });
